fc8ae2f1021224ba74970add3f586bb49eb28fdf
Gisela Ubaldo <g.ubaldo@alumno.um.edu.ar>
2025-04-16 21:37:44 -0300

    Implementar funcion is palindrome

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,3 +1,12 @@
 def is_palindrome(text):
+
+    # Limpiar el texto
+    cleaned = clean_text(text)
+
     
-    pass
+    
+    if len(cleaned) <= 1:
+        return True
+    
+    # Comparar el texto con su reverso
+    return cleaned == cleaned[::-1]
